e6afc135816388dbafe8288b77f5667183a673a9
Madhulika <md_vy@yahoo.com>
2018-08-10 16:24:27 +0530

    Completed till 8.1!

---
+++ StudentBack/src/test/java/com/StudentBack/BShopTest/CartLineTestCase.java
@@ -0,0 +1,78 @@
+package com.StudentBack.BShopTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.StudentBack.BShopDAO.CartLineDAO;
+import com.StudentBack.BShopDAO.UserDAO;
+import com.StudentBack.BShopDTO.Cart;
+import com.StudentBack.BShopDTO.CartLine;
+import com.StudentBack.BShopDTO.User;
+import com.StudentBack.Product.Product;
+import com.StudentBack.ProductDAO.ProductDAO;
+
+public class CartLineTestCase {
+private static AnnotationConfigApplicationContext context;
+	
+	
+	private static CartLineDAO cartLineDAO = null;
+	private static ProductDAO productDAO = null;
+	private static UserDAO userDAO = null;
+	
+	
+	private Product product = null;
+	private User user = null;
+	private Cart cart = null;
+	private CartLine cartLine = null;
+	
+	
+	@BeforeClass
+	public static void init() {
+		context = new AnnotationConfigApplicationContext();
+		context.scan("net.kzn.shoppingbackend");
+		context.refresh();
+		productDAO = (ProductDAO)context.getBean("productDAO");
+		userDAO = (UserDAO)context.getBean("userDAO");
+		cartLineDAO = (CartLineDAO)context.getBean("cartLineDAO");
+				
+	}	
+		
+	@Test
+	public void testAddNewCartLine() {
+		
+		// 1. get the user
+		user = userDAO.getByEmail("absr@gmail.com");
+		
+		// 2. fetch the cart
+		cart = user.getCart();
+		
+		// 3. get the product
+		product = productDAO.get(1);
+		
+		// 4. create a new cartline
+		cartLine = new CartLine();
+		
+		cartLine.setBuyingPrice(product.getUnitPrice());
+		
+		cartLine.setProductCount(cartLine.getProductCount() + 1);
+		
+		cartLine.setTotal(cartLine.getProductCount() * product.getUnitPrice());
+		
+		cartLine.setAvailable(true);
+		
+		cartLine.setCartId(cart.getId());
+
+		cartLine.setProduct(product);
+		
+		assertEquals("Failed to add the cartLine", true, cartLineDAO.add(cartLine));
+		
+		// update the cart
+		cart.setGrandTotal(cart.getGrandTotal() + cartLine.getTotal());
+		cart.setCartLines(cart.getCartLines() + 1);
+		assertEquals("Failed to update the cart", true, cartLineDAO.updateCart(cart));
+}
+
+}
